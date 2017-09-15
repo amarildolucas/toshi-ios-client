4d8e4ec7b6fb10bbe3b89d764d27dd56c419b33a
Robert-Hein Hooijmans <rh.hooijmans@gmail.com>
2017-09-15 15:11:26 +0200

    Make text attributed based on text and match.

--- Toshi/Controllers/PassphraseSignIn/PassphraseSignInCell.swift
+++ Toshi/Controllers/PassphraseSignIn/PassphraseSignInCell.swift
@@ -5,29 +5,61 @@ import TinyConstraints
 class PassphraseSignInCell: UICollectionViewCell {
     
     static let reuseIdentifier: String = "PassphraseSignInCell"
+    private lazy var passwordLabel = UILabel()
+    private(set) var text: String = ""
+    private(set) var match: String?
     
-    lazy var label: UILabel = {
-        let view = UILabel()
-        view.text = "test"
-        view.backgroundColor = .yellow
-        view.font = Theme.regular(size: 16)
-        view.textColor = Theme.darkTextColor
-        
-        return view
-    }()
-    
-    private var contentLayoutGuideTopConstraint: NSLayoutConstraint?
+    override var isSelected: Bool {
+        didSet {
+            contentView.backgroundColor = isSelected ? UIColor.lightGray.withAlphaComponent(0.5) : nil
+            
+            if isSelected {
+                updateAttributedText(text, with: match)
+            } else {
+                updateAttributedText(match ?? "", with: match)
+            }
+        }
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        contentView.backgroundColor = .red
-        
-        contentView.addSubview(label)
-        label.edges(to: contentView, insets: UIEdgeInsets(top: 10, left: 10, bottom: -10, right: -10))
+        contentView.isOpaque = false
+        contentView.addSubview(passwordLabel)
+        passwordLabel.edges(to: contentView, insets: UIEdgeInsets(top: 10, left: 10, bottom: -10, right: -10))
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setText(_ text: String, with match: String? = nil) {
+        self.text = text
+        self.match = match
+        
+        updateAttributedText(text, with: match)
+    }
+    
+    private func updateAttributedText(_ text: String, with match: String? = nil) {
+    
+        let string = text.isEmpty ? "..." : match ?? text
+        
+        let attributes: [String: Any] = [
+            NSFontAttributeName: Theme.regular(size: 17),
+            NSForegroundColorAttributeName: Theme.greyTextColor
+            ]
+        
+        let matchingAttributes: [String: Any] = [
+            NSFontAttributeName: Theme.regular(size: 17),
+            NSForegroundColorAttributeName: Theme.darkTextColor
+            ]
+        
+        let attributedText = NSMutableAttributedString(string: string, attributes: attributes)
+        
+        if let match = match, let matchingRange = (match as NSString?)?.range(of: text, options: [.caseInsensitive, .anchored]) {
+            attributedText.addAttributes(matchingAttributes, range: matchingRange)
+        }
+        
+        passwordLabel.attributedText = attributedText
     }
 }
