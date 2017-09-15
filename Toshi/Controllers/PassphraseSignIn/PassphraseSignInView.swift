fb4501eaa15d42b71e4a16506306bb12c1f58653
Robert-Hein Hooijmans <rh.hooijmans@gmail.com>
2017-09-15 09:41:14 +0200

    Match typed text to passwords library.

--- Toshi/Controllers/PassphraseSignIn/PassphraseSignInView.swift
+++ Toshi/Controllers/PassphraseSignIn/PassphraseSignInView.swift
@@ -6,38 +6,54 @@ final class PassphraseSignInView: UIView {
     
     private lazy var layout: UICollectionViewFlowLayout = {
         let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        layout.minimumInteritemSpacing = 0
-        layout.minimumLineSpacing = 0
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = 10
+        layout.minimumLineSpacing = 10
         layout.estimatedItemSize = UICollectionViewFlowLayoutAutomaticSize
         
         return layout
     }()
     
-    private lazy var collectionView: UICollectionView = {
+    lazy var collectionView: UICollectionView = {
         let view = UICollectionView(frame: .zero, collectionViewLayout: self.layout)
-        view.register(ImageCell.self, forCellWithReuseIdentifier: ImageCell.reuseIdentifier)
-//        view.dataSource = self
+        view.register(PassphraseSignInCell.self, forCellWithReuseIdentifier: PassphraseSignInCell.reuseIdentifier)
         view.backgroundColor = .white
         view.delaysContentTouches = false
-        view.isPagingEnabled = true
+        view.isScrollEnabled = false
         
         return view
     }()
     
-    var passwords: [String]? = nil {
-        didSet {
-            collectionView.reloadData()
-        }
-    }
+    lazy var textField: UITextField = {
+        let view = UITextField()
+        view.font = Theme.regular(size: 16)
+        view.textColor = Theme.darkTextColor
+        view.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        
+        return view
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = Theme.viewBackgroundColor
         
+        addSubview(collectionView)
+        collectionView.edges(to: self)
+        
+        addSubview(textField)
+        textField.left(to: self)
+        textField.bottom(to: self, offset: -300)
+        textField.right(to: self)
+        textField.height(44)
+        
+        textField.becomeFirstResponder()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func textFieldDidChange(_ textField: UITextField) {
+        textField.text = textField.text?.lowercased()
     }
 }
--- Toshi/Controllers/PassphraseSignIn/PassphraseSignInViewController.swift
+++ Toshi/Controllers/PassphraseSignIn/PassphraseSignInViewController.swift
@@ -3,23 +3,49 @@ import UIKit
 import TinyConstraints
 
 final class PassphraseSignInViewController: UIViewController {
-    
+
     var signInView: PassphraseSignInView? { return view as? PassphraseSignInView }
-    
+
+    var activeIndexPath: IndexPath? = nil {
+        didSet {
+            print("activeIndexPath:\(activeIndexPath)")
+            if let cell = activeCell, activeIndexPath != oldValue {
+                signInView?.textField.text = nil
+            }
+        }
+    }
+
+    var activeCell: PassphraseSignInCell? {
+        guard let activeIndexPath = activeIndexPath else { return nil }
+        return signInView?.collectionView.cellForItem(at: activeIndexPath) as? PassphraseSignInCell
+    }
+
+    var passwords: [String]? = nil {
+        didSet {
+            signInView?.collectionView.reloadData()
+        }
+    }
+
     override func loadView() {
         view = PassphraseSignInView()
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        signInView?.collectionView.delegate = self
+        signInView?.collectionView.dataSource = self
+
+        signInView?.textField.delegate = self
+
         loadPasswords { [weak self] in
-            self?.signInView?.passwords = $0
+            self?.passwords = $0
+            self?.activeIndexPath = IndexPath(item: 0, section: 0)
         }
     }
-    
+
     private func loadPasswords(_  completion: @escaping ([String]?) -> Void) {
-        
+
         if let path = Bundle.main.path(forResource: "passwords-library", ofType: "txt") {
             do {
                 let data = try String(contentsOfFile: path, encoding: .utf8)
@@ -29,5 +55,59 @@ final class PassphraseSignInViewController: UIViewController {
                 completion(nil)
             }
         }
+    }
+
+    func match(for text: String) -> String? {
+        guard !text.isEmpty else { return nil }
+
+        let filtered = passwords?.filter {
+            $0.range(of: text, options: [.caseInsensitive, .literal, .anchored]) != nil
+        }
+
+        return filtered?.first
+    }
+}
+
+extension PassphraseSignInViewController: UICollectionViewDelegate {
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        activeIndexPath = indexPath
+    }
+}
+
+extension PassphraseSignInViewController: UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PassphraseSignInCell.reuseIdentifier, for: indexPath)
+
+        return cell
+    }
+}
+
+extension PassphraseSignInViewController: UITextFieldDelegate {
+
+    public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let nsString = textField.text as NSString?
+
+        if let text = nsString?.replacingCharacters(in: range, with: string) {
+
+            print("text:\(text)")
+
+            if let match = match(for: text), let cell = activeCell {
+                cell.label.text = match
+
+                if let indexPath = activeIndexPath {
+                    UIView.performWithoutAnimation {
+                        signInView?.collectionView.reloadItems(at: [indexPath])
+                    }
+                }
+            }
+        }
+
+        return true
     }
 }
