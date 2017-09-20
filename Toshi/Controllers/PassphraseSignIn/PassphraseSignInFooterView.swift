1988d60bb1bb2700840e7f4f4605fd77859055fb
Robert-Hein Hooijmans <rh.hooijmans@gmail.com>
2017-09-20 16:55:33 +0200

    Add buttons to the footer.

--- Toshi/Controllers/PassphraseSignIn/PassphraseSignInFooterView.swift
+++ Toshi/Controllers/PassphraseSignIn/PassphraseSignInFooterView.swift
@@ -11,16 +11,45 @@ class PassphraseSignInFooterView: UIView {
         return view
     }()
     
+    private lazy var signInButton: ActionButton = {
+        let view = ActionButton(margin: 0)
+        view.title = Localized("passphrase_sign_in_button")
+        view.setButtonStyle(.primary)
+        
+        return view
+    }()
+    
+    private lazy var explanationButton: UIButton = {
+        let view = UIButton()
+        view.setTitle(Localized("passphrase_sign_in_explanation_title"), for: .normal)
+        view.setTitleColor(Theme.greyTextColor, for: .normal)
+        
+        return view
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = Theme.borderColor
-        
         addSubview(divider)
+        addSubview(signInButton)
+        addSubview(explanationButton)
+        
         divider.top(to: self)
         divider.left(to: self, offset: 30)
         divider.right(to: self, offset: -30)
         divider.height(1)
+        
+        signInButton.topToBottom(of: divider, offset: 30)
+        signInButton.left(to: self, offset: 30)
+        signInButton.right(to: self, offset: -30)
+        signInButton.height(50)
+        
+        explanationButton.topToBottom(of: signInButton, offset: 5)
+        explanationButton.left(to: self, offset: 30)
+        explanationButton.right(to: self, offset: -30)
+        explanationButton.height(50)
+        explanationButton.bottom(to: self)
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
